e8e7e686e3452359d5bb7be660b3b704359e0035
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-06-16 10:50:54 -0500

    creacion de atributos

--- src/Felinos.java
+++ src/Felinos.java
@@ -1,7 +1,68 @@
 public class Felinos {
+    //Atributos clase Felinos
     private String raza;
     private String nombre;
     private int edad;
     private String habitat;
     private int peso;
+
+    //constructor clase Felino
+
+    public Felinos(String raza, String nombre, int edad, String habitat, int peso) {
+        this.raza = raza;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.habitat = habitat;
+        this.peso = peso;
+    }
+
+    //getters and setters clase felinos
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    //metodos felinos
+    public void rugir() {
+        System.out.println("Este es el metodo rugir de la clase felino");
+    }
+    public void cazar() {
+        System.out.println("Este es el metodo cazar de la clase felino");
+    }
 }
--- src/Leopardo.java
+++ src/Leopardo.java
@@ -1,3 +1,33 @@
-public class Leopardo {
+public class Leopardo extends Felinos{
+    int num_lunares;
+    boolean cazador_nocturno;
+
+    //constructor con la extension de la clase Felinos
+    public Leopardo(String raza, String nombre, int edad, String habitat, int peso, int num_lunares, boolean cazador_nocturno) {
+        super(raza, nombre, edad, habitat, peso);
+        this.num_lunares = num_lunares;
+        this.cazador_nocturno = cazador_nocturno;
+    }
+
+    public int getNum_lunares() {
+        return num_lunares;
+    }
+
+    public void setNum_lunares(int num_lunares) {
+        this.num_lunares = num_lunares;
+    }
+
+    public boolean isCazador_nocturno() {
+        return cazador_nocturno;
+    }
+
+    public void setCazador_nocturno(boolean cazador_nocturno) {
+        this.cazador_nocturno = cazador_nocturno;
+    }
+
+    //Metodo
+    public void trepar() {
+        System.out.println("EL leoardo tiene una gran facilidad de trepar");
+    }
 
 }
